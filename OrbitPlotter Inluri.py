55aaf822d43a6cc5da75a17bd5bb8226d1d047f0
Aaryavarta Dhungel <aaryavarta.dhungel@gmail.com>
2026-06-18 06:57:28 +0000

    Added Density
    Added Stellar Density, did additional formatting of the code

--- OrbitPlotter Inluri.py
+++ OrbitPlotter Inluri.py
@@ -1,5 +1,9 @@
 # Special Thanks to Artifexian, who's Worldsmith 7.01 Calculator was the chief reference for my own spreadsheet, Lightmapper Inluri
-# OrbitPlotter Inluri is the Python translation of the Lightmapper Inluri spreadsheet
+# Worldsmith 7.01 was still a spreadsheet that fundamentally dealt with single-star systems, so it was unable to meet my needs. 
+# Lightmapper Inluri started as my attempt to use it (and my own limited ability) to extend it into binary systems, although it grew into something larger.
+# OrbitPlotter Inluri is the Python translation of the Lightmapper Inluri spreadsheet but has a different scope.
+
+##############################################3 Pre-Section: Define Functions and Formulae #####################################################
 
 def lum(mass): #returns output in Lsol
     if mass < 0.43:
@@ -11,21 +15,25 @@ def lum(mass): #returns output in Lsol
     else:
         return 32000 * mass
 
+def MaxStarAge(mass,lumin): #returns output in Gyrs
+    return 10 * (mass/lumin)
+
+def density(mass,radius): #returns output in Dsol
+    return mass/(radius**3) 
+
 def get_star(name):
     mass = float(input(f"Mass of {name} in Msol: ")) # must be in Msol
     lumin = lum(mass)
     radius = (10**0.003)*(mass**0.724) # from logR = 0.003 + 0.724log(M)
-    return mass, lumin, radius #returns in Msol, Lsol, and Rsol respectively
+    dense = density(mass,radius)
+    return mass, lumin, radius, dense #returns in Msol, Lsol, Rsol and Dsol respectively
 
-def MaxStarAge(mass,lumin): #returns output in Gyrs
-    return 10 * (mass/lumin)
-
-# Section A: Star Parameters
+#####################################3 Section A: Star Parameters #####################################################################
 
 print("\n-------------------------- Section A: Star Parameters --------------\n")
 
-M1, L1, R1 = get_star("Star 1")
-M2, L2, R2 = get_star("Star 2")
+M1, L1, R1, D1 = get_star("Star 1")
+M2, L2, R2, D2 = get_star("Star 2")
 System_Age = float(input("Input the age of your stars in Gyrs ")) # must be in Gyrs
 MaxSysAge = min(MaxStarAge(M1,L1),MaxStarAge(M2,L2))
 
@@ -38,13 +46,15 @@ q = min(M1,M2)/max(M1,M2) #i dont know why i computed this, but i'll just leave 
 # output section
 
 print("\n\n")
-print("Star 1: mass =", M1, "luminosity =", L1, "maximum age =",MaxStarAge(M1,L1), "radius =",R1)
-print("Star 2: mass =", M2, "luminosity =", L2, "maximum age =",MaxStarAge(M2,L2), "radius =",R2)
+print("Star 1: mass =", M1, "luminosity =", L1, "maximum age =",MaxStarAge(M1,L1), "radius =",R1, "density =",D1)
+print("Star 2: mass =", M2, "luminosity =", L2, "maximum age =",MaxStarAge(M2,L2), "radius =",R2, "density =",D2)
 print("Maximum Age of the System",MaxSysAge)
 print("mass ratio q=",q)
 print("\n\n")
 # need to calc density, temperature for Section A to be over
 # Section B covers dynamic of the two stars
+
+###############################################3 Section B: Dynamic of the Two Stars ###################################################################
 
 print("\n-------------------------- Section B: Dynamic of the Two Stars --------------\n")
 
