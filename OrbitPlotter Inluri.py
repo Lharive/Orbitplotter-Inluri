cd88ba280ab648513cd83addc9547dd7987e8427
Aaryavarta Dhungel <aaryavarta.dhungel@gmail.com>
2026-06-18 06:47:44 +0000

    Update OrbitPlotter Inluri.py
    Improved formatting within the code, also improved the readibility of the output.
    
    Additionally, added Stellar radii approximation and output as well.
    
    Includes miscellaneous changes.

--- OrbitPlotter Inluri.py
+++ OrbitPlotter Inluri.py
@@ -14,24 +14,38 @@ def lum(mass): #returns output in Lsol
 def get_star(name):
     mass = float(input(f"Mass of {name} in Msol: ")) # must be in Msol
     lumin = lum(mass)
-    return mass, lumin #returns in Msol and Lsol respectively
+    radius = (10**0.003)*(mass**0.724) # from logR = 0.003 + 0.724log(M)
+    return mass, lumin, radius #returns in Msol, Lsol, and Rsol respectively
 
 def MaxStarAge(mass,lumin): #returns output in Gyrs
     return 10 * (mass/lumin)
 
 # Section A: Star Parameters
-m1, L1 = get_star("Star 1")
-m2, L2 = get_star("Star 2")
+
+print("\n-------------------------- Section A: Star Parameters --------------\n")
+
+M1, L1, R1 = get_star("Star 1")
+M2, L2, R2 = get_star("Star 2")
 System_Age = float(input("Input the age of your stars in Gyrs ")) # must be in Gyrs
-MaxSysAge = min(MaxStarAge(m1,L1),MaxStarAge(m2,L2))
+MaxSysAge = min(MaxStarAge(M1,L1),MaxStarAge(M2,L2))
 
 while System_Age > MaxSysAge:
     print("Your input age is too high as the system will already have evolved past main sequence assumptions. Please retry")
     System_Age = float(input("Input the age of your stars in Gyrs ")) # must be in Gyrs
 
-q = m2/m1 #i dont know why i computed this, but i'll just leave this here for now
+q = min(M1,M2)/max(M1,M2) #i dont know why i computed this, but i'll just leave this here for now
 
-print("Star 1: mass =", m1, "luminosity =", L1, "maximum age =",MaxStarAge(m1,L1))
-print("Star 2: mass =", m2, "luminosity =", L2, "maximum age =",MaxStarAge(m2,L2))
+# output section
+
+print("\n\n")
+print("Star 1: mass =", M1, "luminosity =", L1, "maximum age =",MaxStarAge(M1,L1), "radius =",R1)
+print("Star 2: mass =", M2, "luminosity =", L2, "maximum age =",MaxStarAge(M2,L2), "radius =",R2)
 print("Maximum Age of the System",MaxSysAge)
+print("mass ratio q=",q)
+print("\n\n")
+# need to calc density, temperature for Section A to be over
+# Section B covers dynamic of the two stars
+
+print("\n-------------------------- Section B: Dynamic of the Two Stars --------------\n")
+
 
